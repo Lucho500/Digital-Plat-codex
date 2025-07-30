d0ec6c884d6342b6317d9efbdfd18dd4451e4d30
Lucho500 <luisallauca50@gmail.com>
2025-07-30 16:07:54 +0200

    Fix tests with env variables and polyfills

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,1 +1,15 @@
 import '@testing-library/jest-dom';
+
+// Polyfill ResizeObserver for recharts components in tests
+class ResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+(global as any).ResizeObserver = ResizeObserver;
+
+// Mock scrollIntoView used in MaxAssistant component
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+(window as any).HTMLElement.prototype.scrollIntoView = () => {};
